b8733de8cf4b1164e7e45499fcf7d18a5abedc7a
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-08-22 11:52:57 +0100

    Add basic app skeleton
    Based largly off the buyer app, some config will need adjusting.

---
+++ application.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+from app import create_app
+from dmutils import init_manager
+
+application = create_app(os.getenv('DM_ENVIRONMENT') or 'development')
+manager = init_manager(application, 5007, ['./app/content/frameworks'])
+
+if __name__ == '__main__':
+    manager.run()
---
+++ config.py
@@ -0,0 +1,157 @@
+import os
+import hashlib
+import jinja2
+from dmutils.status import enabled_since, get_version_label
+from dmutils.asset_fingerprint import AssetFingerprinter
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_asset_fingerprint(asset_file_path):
+    hasher = hashlib.md5()
+    with open(asset_file_path, 'rb') as asset_file:
+        buf = asset_file.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
+
+
+class Config(object):
+
+    VERSION = get_version_label(
+        os.path.abspath(os.path.dirname(__file__))
+    )
+    SESSION_COOKIE_NAME = 'dm_session'
+    SESSION_COOKIE_PATH = '/'
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SECURE = True
+
+    PERMANENT_SESSION_LIFETIME = 4*3600
+
+    WTF_CSRF_ENABLED = True
+    WTF_CSRF_TIME_LIMIT = None
+
+    DM_DATA_API_URL = None
+    DM_DATA_API_AUTH_TOKEN = None
+    DM_SEARCH_API_URL = None
+    DM_SEARCH_API_AUTH_TOKEN = None
+    DM_MANDRILL_API_KEY = None
+
+    # matches api(s)
+    DM_SEARCH_PAGE_SIZE = 100
+
+    # This is just a placeholder
+    ES_ENABLED = True
+
+    DEBUG = False
+
+    RESET_PASSWORD_EMAIL_NAME = 'Digital Marketplace Admin'
+    RESET_PASSWORD_EMAIL_FROM = 'enquiries@digitalmarketplace.service.gov.uk'
+    RESET_PASSWORD_EMAIL_SUBJECT = 'Reset your Digital Marketplace password'
+
+    CREATE_USER_SUBJECT = 'Create your Digital Marketplace account'
+    SECRET_KEY = None
+    SHARED_EMAIL_KEY = None
+    RESET_PASSWORD_SALT = 'ResetPasswordSalt'
+    INVITE_EMAIL_SALT = 'InviteEmailSalt'
+
+    ASSET_PATH = '/static/'
+    BASE_TEMPLATE_DATA = {
+        'header_class': 'with-proposition',
+        'asset_path': ASSET_PATH,
+        'asset_fingerprinter': AssetFingerprinter(asset_root=ASSET_PATH)
+    }
+
+    # Feature Flags
+    RAISE_ERROR_ON_MISSING_FEATURES = True
+    FEATURE_FLAGS_NEW_SUPPLIER_FLOW = False
+
+    # LOGGING
+    DM_LOG_LEVEL = 'DEBUG'
+    DM_PLAIN_TEXT_LOGS = False
+    DM_LOG_PATH = None
+    DM_APP_NAME = 'buyer-frontend'
+    DM_DOWNSTREAM_REQUEST_ID_HEADER = 'X-Amz-Cf-Id'
+
+    #: For some frameworks (represented by the keys in this map), we store no framework content. But
+    #: they work just like some other framework (represented by the values in the map) for which we do
+    #: have content available. The map uses slugs to identify the framework pairs, so that we can still
+    #: find the framework data we need (and so that we can avoid trying to load up framework data that
+    #: isn't actually available).
+    DM_FRAMEWORK_CONTENT_MAP = {
+        'g-cloud-4': 'g-cloud-6',
+        'g-cloud-5': 'g-cloud-6',
+    }
+
+    @staticmethod
+    def init_app(app):
+        repo_root = os.path.abspath(os.path.dirname(__file__))
+        template_folders = [
+            os.path.join(repo_root, 'app/templates')
+        ]
+        jinja_loader = jinja2.FileSystemLoader(template_folders)
+        app.jinja_loader = jinja_loader
+
+
+class Test(Config):
+    DEBUG = True
+    DM_PLAIN_TEXT_LOGS = True
+    DM_LOG_LEVEL = 'CRITICAL'
+    WTF_CSRF_ENABLED = False
+
+    DM_DATA_API_URL = "http://wrong.completely.invalid:5000"
+    DM_DATA_API_AUTH_TOKEN = "myToken"
+    DM_SEARCH_API_URL = "http://wrong.completely.invalid:5001"
+    DM_SEARCH_API_AUTH_TOKEN = "myToken"
+
+    DM_MANDRILL_API_KEY = 'MANDRILL'
+    SHARED_EMAIL_KEY = "KEY"
+    SECRET_KEY = "KEY"
+
+    FEATURE_FLAGS_NEW_SUPPLIER_FLOW = enabled_since('2016-11-29')
+
+
+class Development(Config):
+    DEBUG = True
+    DM_PLAIN_TEXT_LOGS = True
+    SESSION_COOKIE_SECURE = False
+    DM_SEARCH_PAGE_SIZE = 5
+
+    DM_DATA_API_URL = "http://localhost:5000"
+    DM_DATA_API_AUTH_TOKEN = "myToken"
+    DM_SEARCH_API_URL = "http://localhost:5001"
+    DM_SEARCH_API_AUTH_TOKEN = "myToken"
+
+    DM_MANDRILL_API_KEY = "not_a_real_key"
+    SECRET_KEY = "verySecretKey"
+    SHARED_EMAIL_KEY = "very_secret"
+
+    FEATURE_FLAGS_NEW_SUPPLIER_FLOW = enabled_since('2016-11-29')
+
+
+class Live(Config):
+    """Base config for deployed environments"""
+    DEBUG = False
+    DM_LOG_PATH = '/var/log/digitalmarketplace/application.log'
+    DM_HTTP_PROTO = 'https'
+
+
+class Preview(Live):
+    FEATURE_FLAGS_NEW_SUPPLIER_FLOW = enabled_since('2017-02-06')
+
+
+class Staging(Live):
+    FEATURE_FLAGS_NEW_SUPPLIER_FLOW = enabled_since('2017-02-07')
+
+
+class Production(Live):
+    FEATURE_FLAGS_NEW_SUPPLIER_FLOW = enabled_since('2017-02-08')
+
+
+configs = {
+    'development': Development,
+    'test': Test,
+
+    'preview': Preview,
+    'staging': Staging,
+    'production': Production,
+}
