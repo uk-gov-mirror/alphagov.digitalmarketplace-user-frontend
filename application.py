cf98ad2eb75851b13cd7f35764a7a284bfe0e2a7
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-08-22 12:08:00 +0100

    Remove extra files from `init_manager()`
    At the moment we don't need the content loader so no need to load it in.

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from app import create_app
 from dmutils import init_manager
 
 application = create_app(os.getenv('DM_ENVIRONMENT') or 'development')
-manager = init_manager(application, 5007, ['./app/content/frameworks'])
+manager = init_manager(application, 5007)
 
 if __name__ == '__main__':
     manager.run()
