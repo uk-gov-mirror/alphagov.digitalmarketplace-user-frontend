2f21dc1bf74df9247301127351d32fb967310bac
Alexey Bezhan <al@fueledbylemons.com>
2017-09-13 17:34:56 +0100

    Add a check for safe url for redirects after login
    After the buyer users have been added to the list of roles the prefix
    checks no longer restrict protocol-relative URLs (eg //example.com),
    allowing the login page to redirect the user to an external domain.
    
    This is adding a `is_safe_url` function based on the one from
    http://flask.pocoo.org/snippets/62/ (it's also used by Notify).
    
    The function is modified to return `False` for empty target URLs.
    
    This also removes per-role redirect restrictions since their original
    purpose was to prevent the open redirects. User role now only affects
    the default redirect location if next_url was not set, otherwise
    next_url is used as is.

--- app/main/helpers/login_helpers.py
+++ app/main/helpers/login_helpers.py
@@ -1,20 +1,32 @@
+from urllib.parse import urlparse, urljoin
+
 from flask_login import current_user
-from flask import redirect, url_for
+from flask import request, redirect, url_for
+
+
+def is_safe_url(next_url):
+    """
+    Return `True` if the url is safe to use in a redirect (ie it doesn't point to
+    an external host or change the scheme from http[s]).
+
+    Returns `False` if url is empty.
+
+    """
+    if not next_url:
+        return False
+
+    app_host = urlparse(request.host_url).netloc
+    next_url = urlparse(urljoin(request.host_url, next_url))
+
+    return next_url.scheme in ['http', 'https'] and next_url.netloc == app_host
 
 
 def redirect_logged_in_user(next_url=None):
-    if current_user.is_authenticated():
-        if current_user.role == 'supplier':
-            if next_url and next_url.startswith('/suppliers'):
-                return redirect(next_url)
-            else:
-                return redirect('/suppliers')
-        if current_user.role.startswith('admin'):
-            if next_url and next_url.startswith('/admin'):
-                return redirect(next_url)
-            else:
-                return redirect('/admin')
-        if next_url and next_url.startswith('/'):
-            return redirect(next_url)
-
-    return redirect(url_for('external.index'))
+    if is_safe_url(next_url):
+        return redirect(next_url)
+    elif current_user.role == 'supplier':
+        return redirect('/suppliers')
+    elif current_user.role.startswith('admin'):
+        return redirect('/admin')
+    else:
+        return redirect(url_for('external.index'))
