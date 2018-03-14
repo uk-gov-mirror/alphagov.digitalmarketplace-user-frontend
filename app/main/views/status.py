e7e3309c0791b750f77d9463c39fa4bbc94e96aa
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2018-03-14 14:46:12 +0000

    Use centralised status method
     ## Summary
    The status endpoint for all of the apps is highly redundant across
    repositories so I've moved it to dm-utils. We now only need to call this
    method from the view stub in each app and pass in a couple of resources
    (mainly the current_app and any api clients) to get a consistent status
    JSON blob back.
    
     ## Ticket
    https://trello.com/c/BBvu6eUk/383-ensure-healthcheck-endpoint-fails-if-low-disk-space

--- app/main/views/status.py
+++ app/main/views/status.py
@@ -1,36 +1,12 @@
-from flask import jsonify, current_app, request
+from flask import request
 
 from .. import main
 from ... import data_api_client
-from dmutils.status import get_flags
+from dmutils.status import get_app_status
 
 
 @main.route('/_status')
 def status():
-    if 'ignore-dependencies' in request.args:
-        return jsonify(
-            status="ok",
-        ), 200
-
-    version = current_app.config['VERSION']
-    api = {
-        'name': '(Data) API',
-        'key': 'api_status',
-        'status': data_api_client.get_status()
-    }
-
-    if api['status'] is None or api['status']['status'] != "ok":
-        return jsonify(
-            {api['key']: api['status']},
-            status="error",
-            version=version,
-            message="Error connecting to the (Data) API.",
-            flags=get_flags(current_app)
-        ), 500
-
-    return jsonify(
-        {api['key']: api['status']},
-        status="ok",
-        version=version,
-        flags=get_flags(current_app)
-    )
+    return get_app_status(data_api_client=data_api_client,
+                          search_api_client=None,
+                          ignore_dependencies='ignore-dependencies' in request.args)
