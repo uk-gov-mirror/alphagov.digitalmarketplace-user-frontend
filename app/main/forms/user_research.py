6ddaf1fd0b9b353d2e60433ad04a74e2755a3a41
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-04-19 10:15:47 +0100

    Fix deprecation warnings for CSRF/FlaskForm

--- app/main/forms/user_research.py
+++ app/main/forms/user_research.py
@@ -1,6 +1,6 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import BooleanField
 
 
-class UserResearchOptInForm(Form):
+class UserResearchOptInForm(FlaskForm):
     user_research_opt_in = BooleanField("Send me emails about opportunities to get involved in user research")
