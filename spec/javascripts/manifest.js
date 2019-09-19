46fb2536d1ca0b7c0306b01c74fe9b51a8c9e8e1
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2019-09-19 17:32:28 +0100

    Add analytics tests, including PII handling

--- spec/javascripts/manifest.js
+++ spec/javascripts/manifest.js
@@ -2,16 +2,19 @@
 var manifest = {
   support : [
     '../../../node_modules/jquery/dist/jquery.js',
-    '../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/analytics/google-analytics-universal-tracker.js',
-    '../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/analytics/analytics.js',
+    '../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/analytics/_pii.js',
+    '../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/analytics/_googleAnalyticsUniversalTracker.js',
+    '../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/analytics/_govukAnalytics.js',
     '../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/analytics/_register.js',
     '../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/analytics/_pageViews.js',
     '../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/analytics/_virtualPageViews.js',
+    '../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/analytics/_trackExternalLinks.js',
     '../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/analytics/_init.js',
     '../../../app/assets/javascripts/analytics/_events.js',
   ],
   test : [
-    '../unit/AnalyticsSpec.js'
+    '../unit/AnalyticsSpec.js',
+    '../unit/piiSpec.js'
   ]
 };
 
--- spec/javascripts/unit/AnalyticsSpec.js
+++ spec/javascripts/unit/AnalyticsSpec.js
@@ -1,9 +1,53 @@
 describe("GOVUK.Analytics", function () {
+  var analytics;
 
   beforeEach(function () {
     window.ga = function() {};
     spyOn(window, 'ga');
   });
 
-  // Add test scenarios here - update the README if any get added!
+  describe('when initialised', function () {
+
+    it('should initialise pageviews, events, track external links and virtual pageviews', function () {
+      spyOn(window.GOVUK.GDM.analytics, 'register');
+      spyOn(window.GOVUK.GDM.analytics.pageViews, 'init');
+      spyOn(window.GOVUK.GDM.analytics.events, 'init');
+      spyOn(window.GOVUK.GDM.analytics.trackExternalLinks, 'init');
+
+      window.GOVUK.GDM.analytics.init();
+
+      expect(window.GOVUK.GDM.analytics.register).toHaveBeenCalled();
+      expect(window.GOVUK.GDM.analytics.pageViews.init).toHaveBeenCalled();
+      expect(window.GOVUK.GDM.analytics.events.init).toHaveBeenCalled();
+      expect(window.GOVUK.GDM.analytics.trackExternalLinks.init).toHaveBeenCalled();
+    });
+  });
+
+  describe('when registered', function() {
+    var universalSetupArguments;
+
+    beforeEach(function() {
+      GOVUK.GDM.analytics.init();
+      universalSetupArguments = window.ga.calls.allArgs();
+    });
+
+    it('configures a universal tracker', function() {
+      expect(universalSetupArguments).toContain(['create', 'UA-49258698-1', {
+        'cookieDomain': document.domain
+      }]);
+      expect(universalSetupArguments).toContain(['send', 'pageview']);
+    });
+    it('configures a cross domain tracker', function() {
+      expect(universalSetupArguments).toContain(['create', 'UA-145652997-1', 'auto', {
+        'name': 'govuk_shared'
+      }]);
+      expect(universalSetupArguments).toContain(['require', 'linker']);
+      expect(universalSetupArguments).toContain(['govuk_shared.require', 'linker']);
+      expect(universalSetupArguments).toContain(['linker:autoLink', [ 'www.gov.uk' ]]);
+      expect(universalSetupArguments).toContain(['govuk_shared.linker:autoLink', [ 'www.gov.uk' ]]);
+      expect(universalSetupArguments).toContain(['govuk_shared.set', 'anonymizeIp', true ]);
+      expect(universalSetupArguments).toContain(['govuk_shared.send', 'pageview']);
+    });
+  });
+
 });
