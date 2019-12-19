8dce606887a3c0c80ae2c749fd0fe32fa61e51cc
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2019-12-19 11:07:59 +0000

    Exclude analytics code from application.js
    Temporarily switching off GA functionality while we review our cookie policy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/show-hide-content.js
 //= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/user-research-consent-banner.js
 //= require ../../../node_modules/digitalmarketplace-govuk-frontend/govuk-frontend/all.js
-//= require _analytics.js'
 //= require _onready.js'
 
 GOVUKFrontend.initAll();
