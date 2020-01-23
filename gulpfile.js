b6f4c722ad8a56e49f6d4e3e21a7a7a57d45310b
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-01-23 17:39:31 +0000

    Fix govuk-frontend images path
    We wanted GOV.UK Frontend image assets to be in a separate folder in our
    static assets directory structure; however, this doesn't fit with the
    [page template for GOV.UK Frontend][1], which expects the favicon (and
    other image assets) to be located in `/static/images`.
    
    This commit changes the gulp task and Sass configuration to put GOV.UK
    Frontend image assets in this location.
    
    [1]: https://github.com/alphagov/govuk-frontend/blob/v2.13.0/src/template.njk#L17

--- gulpfile.js
+++ gulpfile.js
@@ -242,7 +242,7 @@ gulp.task(
   copyFactory(
     'images from the GOVUK frontend',
     govukFrontendImageFolder,
-    staticFolder + '/images/govuk-frontend/'
+    path.join(staticFolder, 'images')
   )
 )
 
