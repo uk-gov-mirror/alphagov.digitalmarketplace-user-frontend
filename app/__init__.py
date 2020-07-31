268881107c1ad0d2fbbbb1f9e2951308cc4aa437
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-07-31 10:47:07 +0100

    Remove govuk_frontend_jinja Flask extension

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ import dmapiclient
 from dmutils import init_app
 from dmutils.user import User
 from dmutils.external import external as external_blueprint
-from govuk_frontend_jinja.flask_ext import init_govuk_frontend
 
 from config import configs
 
@@ -20,9 +19,6 @@ def create_app(config_name):
     application = Flask(__name__,
                         static_folder='static/',
                         static_url_path=configs[config_name].STATIC_URL_PATH)
-
-    # allow using govuk-frontend Nunjucks templates
-    init_govuk_frontend(application)
 
     init_app(
         application,
